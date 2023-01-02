531fb6c8526627d303fdfdd2399e6a9e70cef71e
Vikash Basant <basantvikash360@gmail.com>
2023-01-02 15:23:31 +0530

    Initial commit

---
+++ src/main/java/com/thymeleaf/AddCssJavaScriptImageApplication.java
@@ -0,0 +1,13 @@
+package com.thymeleaf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AddCssJavaScriptImageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AddCssJavaScriptImageApplication.class, args);
+	}
+
+}
